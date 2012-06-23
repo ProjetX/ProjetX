3804f86541b13c3d3ef0f5f2111abde67f3148d6
michael fagno <michael.fagno@free.fr>
2012-06-23 13:20:43 +0200

    Test project

--- src/projetx/Main.java
+++ src/projetx/Main.java
@@ -5,18 +5,30 @@
 
 package projetx;
 
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
 /**
  *
  * @author jonas
  */
+
 public class Main {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        int coucouTuVeuxVoirMaBite;
-    }
+
+     public static void main(String[] args) throws SlickException
+     {
+
+         Game theGame = new Game(null);
+          AppGameContainer app = new AppGameContainer(new Game( Game.title));
+          app.setDisplayMode(Game.width, Game.height, Game.fullscreen);
+          app.setSmoothDeltas(true);
+          app.setTargetFrameRate( Game.fpslimit);
+          app.setShowFPS( Game.showFPS);
+          app.start();
+     }
 
 }
