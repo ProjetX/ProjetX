469b566d59ddacb1c26d0cd29ebffebeaf63e67d
michael fagno <michael.fagno@free.fr>
2012-06-23 16:22:45 +0200

    Vertical gravity added

--- src/projetx/Physics.java
+++ src/projetx/Physics.java
@@ -1,5 +1,6 @@
 package projetx;
 
+import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import org.newdawn.slick.Image;
@@ -34,37 +35,64 @@ public class Physics {
     public void updatePlatforms(ArrayList<Sprite> _platforms){
         platforms = _platforms;
     }
-/*
+
     public void computePhysics(double TimeSinceLastFrame){
 
-        boolean canFall = true;
+        boolean canMove = true;
         Point2D nextPoint;
+        Point2D oldPoint;
+        Sprite plateform;
+        Player otherMovable;
 
 
-       for(Player movable : movables){
+       for(Player currentMovable : movables){
+         oldPoint = currentMovable.getCoords();
 
-        //
-        nextPoint = movable.Position + movable.vitesse * TimeSinceLastFrame;
-
-        //On teste si le movable peut tomber
-        for(Sprite plateform: platforms){
-            
+        nextPoint = scalarCross(addVectors(currentMovable.getCoords(), currentMovable.getSpeed()), TimeSinceLastFrame);
+        currentMovable.setCoords(nextPoint);
+         
+        //On teste si le movable collide dans sa prochaine position
+        for(int i = 0; i < platforms.size() && canMove == true; i++){
+            plateform = platforms.get(i);
+            canMove = !IsColliding(currentMovable, plateform);
         }
-        for(Object movable : movables){
+        for(int i = 0; i < movables.size() && canMove == true; i++){
+            otherMovable = movables.get(i);
+            if(otherMovable != currentMovable){
+                canMove = !IsColliding(currentMovable, otherMovable);
+            }
+        }
 
+        //Si on ne peut pas bouger
+        if(canMove == false){
+            currentMovable.setCoords(oldPoint);
         }
        }
     }
 
-    private boolean IsColliding(){
+    private boolean IsColliding(Sprite obj1, Sprite obj2){
         boolean isColliding = false;
 
         //nextPoint
-            //if( plateform.getCoords().getX() < nextPoint.x)
+        if( (obj1.getCoords().getX() + obj1.getImage().getWidth() > obj2.getCoords().getX() && obj1.getCoords().getX() + obj1.getImage().getWidth() < obj2.getCoords().getX() + obj2.getImage().getWidth()) || (obj1.getCoords().getX() > obj2.getCoords().getX() && obj1.getCoords().getX() < obj2.getCoords().getX() + obj2.getImage().getWidth() ) ){ //Aligné suivant y
+            if( (obj1.getCoords().getY() + obj1.getImage().getHeight() > obj2.getCoords().getY() && obj1.getCoords().getY() + obj1.getImage().getHeight() < obj2.getCoords().getY() + obj2.getImage().getHeight()) || (obj1.getCoords().getY() > obj2.getCoords().getY() && obj1.getCoords().getY() < obj2.getCoords().getY() + obj2.getImage().getHeight() )){ //Aligné suivant x
+                isColliding = true;
+            }
+        }
 
         return isColliding;
     }
 
-*/
+    private Point2D addVectors(Point2D a, Point2D b){
+        Point2D c = new Point2D.Double();// a.getX() + b.getX(), a.getY() + b.getY());
+        c.setLocation(a.getX() + b.getX(), a.getY() + b.getY());
+        return c;
+    }
+
+    private Point2D scalarCross(Point2D a, double l){
+        Point2D b= new Point2D.Double(a.getX()*l,a.getY()*l);
+        return b;
+    }
+
 
 }
